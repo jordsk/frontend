2473a0bdf9d0a52c142a0aa9ea757948c1208c05
jordsk <jordan_806@hotmail.com>
2022-09-28 13:14:34 +0100

    day3break

---
+++ Nationwide/day 3/script.js
@@ -0,0 +1,11 @@
+function subCalc(num1, num2){
+    return num1 - num2;
+}
+console.log(subCalc(10,1000));
+
+const welcome = function(name,age,gender){
+    return console.log(`My name is ${name}, i am ${age} years old and of gender ${gender}`);
+    }
+   
+    welcome("Vegeta, Prince of all Saiyans",28,"Male");
+
