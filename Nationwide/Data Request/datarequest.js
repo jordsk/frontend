1a6c49335e905a985975f613ae3f3d6003dcd36e
jordsk <jordan_806@hotmail.com>
2022-09-30 14:23:36 +0100

    twothreetwo

--- Nationwide/Data Request/datarequest.js
+++ Nationwide/Data Request/datarequest.js
@@ -5,12 +5,14 @@ const URL = "datarequest.json";
 const URL2 = "https://reqres.in/";
 
 fetch(URL2)
-.then(function(response){
-    return response.json();
-})
-.then(function (data){
-    console.log(data);
-})
-.catch(function (err){
-    console.error(err);
-})
+fetch('https://reqres.in/api/users?page=2')
+     .then((response) => {
+         if (response.status !== 200) {
+             console.log(`Looks like there was a problem.Status Code: ${ response.status }`);
+             return;
+         }
+     response.json()
+     .then(data => console.log(data));         
+     })
+     .catch(err => console.error(`Fetch Error :-S ${err}`));
+
