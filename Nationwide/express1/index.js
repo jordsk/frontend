c082e938d7a89ed8bb90e2700089bcdb3c95ec77
jordsk <jordan_806@hotmail.com>
2022-10-06 16:12:59 +0100

    tekfieddiyat

--- Nationwide/express1/index.js
+++ Nationwide/express1/index.js
@@ -1,16 +1,59 @@
 const express = require("express");
-const app = express ();
 
-app.get('./', (req, res) => res.send('Hello, my name is JK!'));
+const bodyParser = require("body-parser");
+
+const app = express ();
 
 const names = ['Jord', 'Dyl', 'Harry', 'Sam', 'James'];
 
-app.post('/CreateTEAM', (req, res) => console.log('CreateTEAM'));
+app.use(bodyParser.json());
+
+app.use((req, res, next) => {
+    console.log("Request recevied at", new Date());
+   return next(); 
+})
+
+app.use((req, res, next) => {
+    console.log("I just exist to be an example");
+    return next();
+})
+
+
+app.get("/hello", (req, res) => res.send('Hello, my name is JK!'));
+
+
+app.get("/hello", (req, res) => {
+    res.send("Hello, World!")
+});
 
 app.get('/getAll', (req, res) => res.send(names));
 
-app.get('/get/:id', (req, res) => res.send(names[req.params.id]));
+app.get('/getNames/:id', (req, res) => res.send(names[req.params.id]));
 
+
+const deleteMiddleware = (req, res, next) => {
+    console.log("You're trying to DELETE A DUCK? YOU MONSTER!!!");
+    next();
+}
+
+app.post('/CreateTEAM', (req, res) => console.log('CreateTEAM'));
+
+app.post("/createNames", (req, res) => {
+    names.push(req.body);
+    res.status(201).send(names);
+});
+
+app.patch("/updateNames/:id", (req, res) => {
+    console.log("ID:", req.params.id);
+    console.log("QUERY:", req.query);
+    res.send();
+})
+
+app.delete("/removeNames/:id", deleteMiddleware, (req, res) => {
+    console.log("ID:", req.params.id);
+
+    res.send(names.splice(req.params.id));
+});
 
 app.delete("/remove/:id", (req, res) => res.send(names.splice(req.params.id, i)));
 
@@ -21,6 +64,11 @@ app.put("/replace/:id", (req, res) =>{
 })
 
 
-//onst server = app.listen(4004, () => console.log(`Server successfully started on port ${server.address().port}`));
+app.use((req, res, next) => {
+    //do something
+    next();
+})
 
-app.listen(4000, () => console.log("Welcome to the house of FUN!"));
+//const server = app.listen(4001, () => console.log(`Server successfully started on port ${server.address().port}`));
+
+app.listen(4001, () => console.log("Welcome to the house of FUN!"));
