a98f891c33f67260b9235ac4914c09fb739db3f8
jordsk <113518283+jordsk@users.noreply.github.com>
2022-10-07 10:31:40 +0100

    Delete Nationwide/modules-practise directory

--- Nationwide/modules-practise/cow.js
+++
@@ -1,5 +0,0 @@
-const cow = require('cowsay');
-
-(Moo) => cow.says({'Moo': Moooooo})
-
-module.exports.speak = (Moo) => cow.says({'Moo': Moooooo})
--- Nationwide/modules-practise/index.js
+++
@@ -1,3 +0,0 @@
-const cow = require('./cow');
-
-console.log(cow.speak('Moo'));
