82c9a7456f8bb9267c5daa67ac1f00415a18eb29
jordsk <jordan_806@hotmail.com>
2022-10-13 15:31:46 +0100

    endofthurs

--- Nationwide/react/react-demo/src/ComponentsWithProps.jsx
+++ Nationwide/react/react-demo/src/ComponentsWithProps.jsx
@@ -1,12 +1,28 @@
 import logo from './logo.svg';
 import './App.css';
 import PropComp from './Hello';
+import Products from './MyComponent';
 
-Function ComponentsWithProps () {
-const ComponentsWithProps = () = 
+Function ComponentsWithProps() {
+const ComponentsWithProps = () => <h1>Yo!</h1>
 
 return (
-
+    <div className="App">
+        <Heading/>
+        <Person/>
+        <Products/>
+        <Products
+        name="Pepsi"
+        price={1.99}
+        descript="another drink"
+        />
+        <Products
+        name="Fanta"
+        prcice={1.00}
+        descript="again, another drink"
+        />
+        <Parent/>
+    </div>
 )
 };
 export default App;
---
+++ Nationwide/react/react-demo/src/MyComponent.jsx
@@ -0,0 +1,13 @@
+function MyComponent (props) {
+    const item =
+    
+
+    return(
+        <h1></h1>
+        <p></p>
+        <p></p>
+        <p></p>
+
+    )
+}
+export default Products;
