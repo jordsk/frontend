e8adf5d9f8c9f93bb7db2537fa06126efccde73e
jordsk <jordan_806@hotmail.com>
2022-10-24 17:23:17 +0100

    endofday1hackathon

--- Nationwide/hackathon/main.js
+++ Nationwide/hackathon/main.js
@@ -16,5 +16,19 @@ window.addEventListener('load', () => {
 
         const pokemon_el = document.createElement("div");
         pokemon_el.classList.add("pokemon");
+
+        const pokemon_content_el = document.createElement("div");
+        pokemon_content_el.classList.add("content");
+
+        pokemon_el.appendChild(pokemon_content_el);
+
+        const pokemon_input_el = document.createElement("input");
+        pokemon_input_el.classList.add("pokemons");
+        pokemon_input_el.type = "text";
+        pokemon_input_el.value = "pokemon";
+
+        pokemon_content_el.appendChild(pokemon_input_el);
+
+        list_el.appendChild(pokemon_el);
     })
 })
