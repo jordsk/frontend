d19ce1236eca79438f0dd10fbd5e4a3568e5c31a
jordsk <jordan_806@hotmail.com>
2022-10-25 15:58:27 +0100

    endofday2hack

--- Nationwide/hackathon/server/server.js
+++ Nationwide/hackathon/server/server.js
@@ -1,8 +1,22 @@
-const express = require('express')
-const app = express()
+const express = require('express');
+let request = require("request");
+const app = express();
+const port = 5000;
 
 app.get("/api", (req, res) => {
     res.json({"pokemons":["pokemonOne", "pokemonTwo", "pokemonThree,"] })
 })
+
+app.get ("/", (req, res) => res.send("Welcome to my Pokemon Store"));
+app.get("/newEndpoint", (req, res) => res.send("This is my Pokemon Store endpoint"));
+
+app.get("/getNewpokemon", (req, res) => {
+    request("http://localhost:5000", function(error, response, body) {
+        if (!error && response.statusCode == 200) {
+            res.send(body);
+        }
+    }
+    );
+});
 
 app.listen(5000, () => { console.log("server started on port 5000") })
